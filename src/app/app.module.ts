e7e91383f9cb94b120e68ae77f66cae1d24145a2
Tomek Bujakowski <bujakowski@gmail.com>
2017-08-08 21:18:26 +0200

    Generate item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
