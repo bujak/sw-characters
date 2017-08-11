2207c625edf24492bec77332621fa17c7b410b1f
Tomek Bujakowski <bujakowski@gmail.com>
2017-08-11 15:02:06 +0200

    Implement star-wars service in item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {StarWarsService} from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
@@ -7,14 +8,19 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() character;
-  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
-  constructor() { }
+  @Output() sideAssigned = new EventEmitter<{ name: string, side: string }>();
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit() {
   }
 
   onAssign(side) {
     // this.character.side = side;
-    this.sideAssigned.emit({name: this.character.name, side: side});
+    // this.sideAssigned.emit({name: this.character.name, side: side});
+    this.swService.onSideChosen({name: this.character.name, side: side});
   }
 }
