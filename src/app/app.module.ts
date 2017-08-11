c36ce9824acf26b9d1786754d14fa074efbe089e
Tomek Bujakowski <bujakowski@gmail.com>
2017-08-11 15:01:36 +0200

    Add star-wars in providers in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ItemComponent } from './item/item.component';
 import { ListComponent } from './list/list.component';
+import {StarWarsService} from './star-wars.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StarWarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
