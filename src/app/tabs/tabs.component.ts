dc9e5634eb8f79e656474fb5621b1ce0ccb5c7ff
Tomek Bujakowski <bujakowski@gmail.com>
2017-08-11 13:04:36 +0200

    Implement tabs component

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-
+  characters = [
+    {name: 'Yoda', side: ''},
+    {name: 'Palpatine', side: ''}
+    ];
   constructor() { }
 
+  chosenList = 'all';
   ngOnInit() {
   }
 
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
+  }
 }
