cf81b7ee7f31563517c4fe9939123e8f3ebe0f0a
Tomasz Bujakowski <bujakowski@gmail.com>
2017-08-17 13:12:31 +0200

    Add paths for side choosing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { ItemComponent } from './item/item.component';
-import { ListComponent } from './list/list.component';
-import { StarWarsService } from './star-wars.service';
+import {AppComponent} from './app.component';
+import {TabsComponent} from './tabs/tabs.component';
+import {ItemComponent} from './item/item.component';
+import {ListComponent} from './list/list.component';
+import {StarWarsService} from './star-wars.service';
 import {LogService} from './log.service';
-import { CreateCharacterComponent } from './create-character/create-character.component';
+import {CreateCharacterComponent} from './create-character/create-character.component';
 import {FormsModule} from '@angular/forms';
 import {HeaderComponent} from './header/header.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes = [
-  {path: '', component: TabsComponent},
+  {
+    path: 'characters', component: TabsComponent, children: [
+    {path: '', redirectTo: 'all', pathMatch: 'full'},
+    {path: ':side', component: ListComponent}
+  ]
+  },
   {path: 'add', component: CreateCharacterComponent},
-  {path: '**', redirectTo: '/'}
+  {path: '**', redirectTo: '/characters'}
 ];
 
 @NgModule({
@@ -35,4 +40,5 @@ const routes = [
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
