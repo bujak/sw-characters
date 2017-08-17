15b4df3496aaacda03a40d100938805a3313e175
Tomasz Bujakowski <bujakowski@gmail.com>
2017-08-17 16:20:45 +0200

    Add ngOnDestroy method with unsubscribe in list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {StarWarsService} from '../star-wars.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   @Input() characters;
   activatedRoute: ActivatedRoute;
   swService: StarWarsService;
   loadedSide = 'all';
+  subscription: Subscription;
 
   constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
     this.activatedRoute = activatedRoute;
@@ -25,12 +27,15 @@ export class ListComponent implements OnInit {
         this.loadedSide = params.side;
       }
     );
-    this.swService.charactersChanged.subscribe(
+    this.subscription = this.swService.charactersChanged.subscribe(
       () => {
         this.characters = this.swService.getCharacters(this.loadedSide);
-
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
