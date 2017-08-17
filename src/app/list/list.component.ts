0829d032289a49b06b64a77cd05ef9c6e0e92ee6
Tomasz Bujakowski <bujakowski@gmail.com>
2017-08-17 14:27:40 +0200

    Inject activated route in list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,14 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ListComponent implements OnInit {
   @Input() characters;
-  constructor() { }
+  activatedRoute: ActivatedRoute;
+
+  constructor(activatedRoute: ActivatedRoute) {
+    this.activatedRoute = activatedRoute;
+  }
 
   ngOnInit() {
+
   }
 
 }
