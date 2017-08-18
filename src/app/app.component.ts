b39bc52fbb02964387a9f7f19bff27f464eaddb7
Tomasz Bujakowski <bujakowski@gmail.com>
2017-08-18 19:08:09 +0200

    Move fetchCharacters call from list component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StarWarsService} from './star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  private swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+  ngOnInit(): void {
+    this.swService.fetchCharacters();
+
+  }
+
+
 }
